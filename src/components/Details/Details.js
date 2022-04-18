c6d1b8f20b26a78720cdd7364102fac7639bf4cd
moyen90 <moyen@connekt.studio>
2022-04-19 00:41:31 +0600

    simple changed

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Details = () => {
     return (
         <div className='details'>
-            <h1>This is Details</h1>
+            <div className='work'>
+                <h1>Work Experience</h1>
+            </div>
         </div>
     );
 };
