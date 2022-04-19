2a3c299c93587e16c65aaafad0b7ba118a1839f8
moyen90 <moyen@connekt.studio>
2022-04-20 00:35:59 +0600

    work experience added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,7 +5,23 @@ const Details = () => {
         <div className='details'>
             <div className='work'>
                 <h1>Work Experience<span></span></h1>
+            </div>
+            <div className='work-space'>
+                <div>
+                    <div class="timelines">
+                        <div class="timeline experience">
+                            <div class="timeline-items">
+                                <div class="timeline-item">
+                                    <h1>Sofware developer</h1>
+                                    <h3>at CONNEKT STUDIO</h3>
+                                    <time datetime="2020/2022"> Jan 2022 &ndash; Present</time>
+                                    <div class="location">Bagura,Rajshahi,Bangladesh</div>
+                                </div>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
             </div>
         </div>
     );
